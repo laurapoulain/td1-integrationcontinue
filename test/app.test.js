506b3bb3681d7da4080f9c83bdcf3601ed8cb640
Laura Poulain <laurapoulain05@gmail.com>
2025-11-03 15:12:26 +0100

    Test unitaire de la surface calculée du triangle

--- test/app.test.js
+++ test/app.test.js
@@ -4,18 +4,18 @@ const expect = require('chai').expect;
 describe('Testing the Triangle Functions', function() {
     it('1. The base of the Triangle', function(done) {
         let t1 = new Triangle(10, 5);
-        expect(c1.getBase()).to.equal(10);
+        expect(t1.getBase()).to.equal(10);
         done();
     });
     
-    it('2. The surface area of the Cube', function(done) {
-        let c2 = new Cube(5);
-        expect(c2.getSurfaceArea()).to.equal(150);
-        done();
+    it('2. The surface area of the Triangle', function(done) {
+        let t2 = new Triangle(6,10);
+        expect(t2.getSurfaceArea()).to.equal(6 * 10 * 0.5);    //surface = base * height * 0.5
+        done()
     });
     
     it('3. The volume of the Cube', function(done) {
-        let c3 = new Cube(7);
+        let c3 = new Cube(1,3);
         expect(c3.getVolume()).to.equal(343);
         done();
     });
